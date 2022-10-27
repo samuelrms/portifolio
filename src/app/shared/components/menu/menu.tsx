55b9514bdecf54b0740bba24c353f650429fd0b4
samuel <samuelaoliveiraramos@gmail.com>
2022-10-27 17:45:15 -0300

    Fix: toolbar

--- src/app/shared/components/menu/menu.tsx
+++ src/app/shared/components/menu/menu.tsx
@@ -1,4 +1,13 @@
-import { Paper, Stack } from "@mui/material";
+import { useState } from "react";
+import { Settings } from "@mui/icons-material";
+import {
+  Paper,
+  Stack,
+  SpeedDial,
+  Icon,
+  Backdrop,
+  SpeedDialAction,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { routes } from "../../../routes/routes";
 import { Logo } from "../../assets";
@@ -6,6 +15,9 @@ import { ListItemLink } from "./menuLink";
 
 export const MenuNavigation = () => {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const handleClick = () => {
     navigate("/home");
@@ -21,10 +33,11 @@ export const MenuNavigation = () => {
         paddingY: 1,
         alignItems: "center",
         justifyContent: "space-between",
+        position: "relative",
       }}
     >
       <Logo onClick={handleClick} />
-      <Stack gap={0.5} flexDirection={"row"}>
+      <Stack gap={0.5} flexDirection={"row"} mr={8}>
         {routes.map((data, index) => (
           <ListItemLink
             key={index}
@@ -33,6 +46,32 @@ export const MenuNavigation = () => {
             onClick={() => undefined}
           />
         ))}
+        <Backdrop open={open} />
+        <SpeedDial
+          sx={{
+            position: "absolute",
+            right: 0,
+            top: 4,
+          }}
+          ariaLabel="config"
+          direction="down"
+          icon={<Settings fontSize="medium" />}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          open={open}
+        >
+          <SpeedDialAction
+            key={"teste"}
+            icon={
+              <Icon color="primary" fontSize="medium">
+                light
+              </Icon>
+            }
+            tooltipTitle={"Alterar tema"}
+            tooltipOpen
+            onClick={handleClose}
+          />
+        </SpeedDial>
       </Stack>
     </Paper>
   );
