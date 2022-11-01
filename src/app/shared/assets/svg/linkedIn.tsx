839098a8f284d1be252761fa1135ed64dd98b9d4
samuelrms <samuelaoliveiraramos@gmail.com>
2022-11-01 06:41:41 -0300

    Add: svg linkedIn, github and change paid svg folder structure

---
+++ src/app/shared/assets/svg/linkedIn.tsx
@@ -0,0 +1,17 @@
+import { SVGProps } from "react";
+
+export const LinkedIn = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M16 0c8.837 0 16 7.164 16 16s-7.163 16-16 16S0 24.836 0 16 7.163 0 16 0Z"
+      fill="#fff"
+      fillOpacity={0.5}
+    />
+  </svg>
+);
