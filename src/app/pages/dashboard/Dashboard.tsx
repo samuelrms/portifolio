0a81e2a47bfc94fe155ef8f5f74cab1af5981bb8
samuelrms <samuelaoliveiraramos@gmail.com>
2022-11-01 22:47:54 -0300

    Fix: add functions redirect and download curriculum

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -8,6 +8,18 @@ import { Button, Container, Paper, Stack, Typography } from "@mui/material";
 import { Avatar } from "../../shared/assets";
 
 export const Dashboard = () => {
+  const onDownload = () => {
+    fetch("curriculum.pdf").then((response) => {
+      response.blob().then((blob) => {
+        const fileURL = window.URL.createObjectURL(blob);
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "Curriculum_Samuel_Ramos.pdf";
+        alink.click();
+      });
+    });
+  };
+
   return (
     <Container maxWidth="xl">
       <Stack flexDirection={"row"} justifyContent={"space-between"}>
@@ -27,8 +39,20 @@ export const Dashboard = () => {
           </Typography>
           <Typography color={"#FFFFFF80"}>Front-end developer </Typography>
           <Stack flexDirection={"row"} pt={2}>
-            <LinkedIn sx={{ cursor: "pointer", color: "#FFFFFF80", mr: 2 }} />
-            <GitHub sx={{ cursor: "pointer", color: "#FFFFFF80" }} />
+            <Stack
+              component={"a"}
+              href="https://www.linkedin.com/in/samuelaoramos/"
+              target={"_blank"}
+            >
+              <LinkedIn sx={{ cursor: "pointer", color: "#FFFFFF80", mr: 2 }} />
+            </Stack>
+            <Stack
+              component={"a"}
+              href="https://github.com/samuelrms"
+              target={"_blank"}
+            >
+              <GitHub sx={{ cursor: "pointer", color: "#FFFFFF80" }} />
+            </Stack>
           </Stack>
         </Stack>
         <Stack pt={5.5}>
@@ -36,19 +60,32 @@ export const Dashboard = () => {
         </Stack>
         <Stack maxWidth={"200px"} mt={39} width={"100%"}>
           <Stack justifyContent={"flex-end"} flexDirection={"row"} pb={1}>
-            <Typography sx={{ cursor: "pointer" }}>Baixar CV </Typography>
-            <ArrowDownwardSharp sx={{ cursor: "pointer", ml: 0.5 }} />
+            <Stack
+              flexDirection={"row"}
+              sx={{ cursor: "pointer" }}
+              onClick={onDownload}
+            >
+              <Typography>Baixar CV </Typography>
+              <ArrowDownwardSharp sx={{ ml: 0.5 }} />
+            </Stack>
           </Stack>
           <Stack
-            component={Button}
-            flexDirection={"row"}
-            borderRadius={4}
-            p={1}
-            border={"1px solid rgba(114, 255, 79, 0.5)"}
-            sx={{ cursor: "pointer" }}
+            component={"a"}
+            href="https://api.whatsapp.com/send?1=pt_BR&phone=5534988676166"
+            target={"_blank"}
+            sx={{ textDecoration: "none" }}
           >
-            <WhatsApp sx={{ color: "#72FF4F" }} fontSize="small" />
-            <Typography pr={0.5}>Vamos conversar </Typography>
+            <Stack
+              component={Button}
+              flexDirection={"row"}
+              borderRadius={4}
+              p={1}
+              width={"100%"}
+              border={"1px solid rgba(114, 255, 79, 0.5)"}
+            >
+              <WhatsApp sx={{ color: "#72FF4F" }} fontSize="small" />
+              <Typography pl={1}>Vamos conversar </Typography>
+            </Stack>
           </Stack>
         </Stack>
       </Stack>
