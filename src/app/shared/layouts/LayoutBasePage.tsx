85b611995fc7908a9910438fcdc881ec27440e36
samuelrms <samuelaoliveiraramos@gmail.com>
2022-11-01 22:47:08 -0300

    Add: footer

--- src/app/shared/layouts/LayoutBasePage.tsx
+++ src/app/shared/layouts/LayoutBasePage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import { GitHub, LinkedIn } from "@mui/icons-material";
 
 interface LayoutBasePageProps {
   children: React.ReactNode;
@@ -14,8 +15,50 @@ export const LayoutBasePage: React.FC<LayoutBasePageProps> = ({ children }) => {
       maxWidth={"xl"}
       m={"0 auto"}
     >
-      <Box padding={3} flex={1} overflow="auto">
-        {children}
+      <Box flex={1} overflow="auto">
+        <Box padding={3}>{children}</Box>
+        <Stack
+          sx={{
+            paddingX: 2,
+            paddingY: 1,
+            justifyContent: "center",
+            maxWidth: "xl",
+          }}
+          height={90}
+          bgcolor={"#72FF4F1A"}
+        >
+          <Stack
+            flexDirection={"row"}
+            width={"50%"}
+            justifyContent={"space-between"}
+          >
+            <Typography color={"#FFFFFF80"}>
+              Copyright © Samuel Ramos · 2022
+            </Typography>
+            <Stack flexDirection={"row"}>
+              <Stack
+                component={"a"}
+                href="https://www.linkedin.com/in/samuelaoramos/"
+                target={"_blank"}
+              >
+                <LinkedIn
+                  fontSize="large"
+                  sx={{ cursor: "pointer", color: "#FFFFFF80", mr: 2 }}
+                />
+              </Stack>
+              <Stack
+                component={"a"}
+                href="https://github.com/samuelrms"
+                target={"_blank"}
+              >
+                <GitHub
+                  fontSize="large"
+                  sx={{ cursor: "pointer", color: "#FFFFFF80" }}
+                />
+              </Stack>
+            </Stack>
+          </Stack>
+        </Stack>
       </Box>
     </Box>
   );
